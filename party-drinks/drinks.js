dc4d4fded7815fef76f350486c639f21ddf5ce0e
Snoeck Sven <snoeck.sven@live.be>
2023-02-06 10:15:15 +0100

    drinks.js and github pages

---
+++ party-drinks/drinks.js
@@ -0,0 +1,36 @@
+const drinks = [
+  {
+    Name: "Hugo",
+    Price: "5",
+  },
+  {
+    Name: "Martini",
+    Price: "7",
+  },
+  {
+    Name: "Margarita",
+    Price: "7",
+  },
+  {
+    Name: "Manhattan",
+    Price: "9",
+  },
+  {
+    Name: "Earl Grey tea",
+    Price: "2",
+  },
+  {
+    Name: "Herbal tea",
+    Price: "2",
+  },
+  {
+    Name: "Ginger ale",
+    Price: "3",
+  },
+  {
+    Name: "Lemonade",
+    Price: "3",
+  },
+];
+
+export default drinks;
